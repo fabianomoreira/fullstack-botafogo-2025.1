a44030ba711f3a0ecb4d8359ff6e756d88d7b1fe
Fabiano <fabiano.pinho@rj.senac.br>
2026-03-16 21:18:35 -0300

    Implementação do service

--- flutter/projetos/contatos_app/lib/services/api_service.dart
+++ flutter/projetos/contatos_app/lib/services/api_service.dart
@@ -1,3 +1,18 @@
+import 'dart:convert';
+import 'package:contatos_app/models/pessoa.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService{
   static const String baseURL = 'http://localhost:8080/pessoas';
+
+  static Future<List<Pessoa>> getPessoas() async {
+    final response = await http.get(Uri.parse(baseURL));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => Pessoa.fromJson(data)).toList();
+    } else {
+      throw Exception('Erro ao carregar pessoas: ${response.statusCode}');
+    }
+  }
 }
